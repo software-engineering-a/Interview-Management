3a8150a40a1e04fc331a34ea22bac6503c3d7cbd
masif4347 <l134347@lhr.nu.edu.pk>
2019-04-28 21:41:19 +0500

    date views updated

--- RMS/Models/InterviewManagement.cs
+++ RMS/Models/InterviewManagement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace RMS.Models
     public class InterviewManagement
     {
         public int ID { get; set; }
+
         public int UID { get; set; }
+        [Display(Name ="Interview Date")]
         public DateTime interview_date { get; set; }
+        [Display(Name = "Interview Time")]
+
         public TimeSpan time { get; set; }
         public string status { get; set; }
     }
