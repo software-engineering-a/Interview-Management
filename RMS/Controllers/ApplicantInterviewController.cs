2512918747713e61888d83fbdcb75ebe1c41ff89
masif4347 <l134347@lhr.nu.edu.pk>
2019-04-28 23:27:08 +0500

    Applicant Side Covered

---
+++ RMS/Controllers/ApplicantInterviewController.cs
@@ -0,0 +1,40 @@
+﻿using RMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Controllers
+{
+    public class ApplicantInterviewController : Controller
+    {
+        private DBConnection db = new DBConnection();
+
+        // GET: HRMInterview
+        public ActionResult Index()
+        {
+            return View(db.interview_mangement.ToList());
+        }
+        public ActionResult update_status(int? id,string status)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InterviewManagement interviewManagement = db.interview_mangement.Find(id);
+            if (interviewManagement == null)
+            {
+                return HttpNotFound();
+            }
+            interviewManagement.status = status;
+
+            db.Entry(interviewManagement).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
